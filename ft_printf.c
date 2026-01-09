e56ce8f263d2d98fa7e05bd7f48fd7477e5a155a
akhalidi <akhalidi@gmail.com>
2026-01-09 02:45:23 +0100

    loading

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,28 @@
 #include "libft.h"
 #include <stdarg.h>
 #include <stdio.h>
-void	check_type(char indice, va_list ap)
+
+int	ft_putnbr_base(int nbr, char *base)
 {
-	if (indice == 'c')
+	int	len;
+
+	len = ft_strlen(base);
+	if(nbr < -1)
+		nbr = ft_putnbr()
+}
+int	check_format(char indice, va_list ap)
+{
+	int	count;
+
+	count = 0;
+	if (indice == NULL)
+		return (0);
+	else if (indice == 'c')
 		ft_putchar_fd(va_arg(ap, int), 1);
-	else if (indice == 'd')
-		ft_putnbr_fd(va_arg(ap, int), 1);
+	else if (indice == 'd' || indice == 'i'|| indice == 'u')
+		ft_putnbr_base(va_arg(ap, int), "0123456789");
+	else if (indice == 'x' || indice == 'X')
+		ft_putnbr_base(va_arg(ap, int), "0123456789abcdef");
 	else if (indice == 's')
 		ft_putstr_fd(va_arg(ap, char *), 1);
 //	return (count);
@@ -29,7 +45,6 @@ int	ft_printf(const char *format, ...)
 		{
 			i++;
 			check_type(format[i], ap);
-			i++;
 		}
 		ft_putchar_fd(format[i], 1);
 		count++;
