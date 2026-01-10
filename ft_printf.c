caaf01f55c76e1ad73b5bfb680563a0a0380e5de
Abdellatif Khalidi <akhalidi0411@gmail.com>
2026-01-10 02:21:24 +0100

    modified

--- ft_printf.c
+++ ft_printf.c
@@ -1,31 +1,60 @@
-#include "libft.h"
+//#include "libft.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdio.h>
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while(s[i])
+		i++;
+	return i;
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 int	ft_putnbr_base(int nbr, char *base)
 {
 	int	len;
+	int	count;
+	char	result;
 
 	len = ft_strlen(base);
-	if(nbr < -1)
-		nbr = ft_putnbr()
+	count = 0;
+	if (nbr < -1)
+		nbr *= -1;
+	if (nbr < len)
+	{
+		result = nbr % len + '0';
+		ft_putchar(result);
+	}
+	if (nbr > len)
+		count += ft_putnbr_base(nbr / len, base);
+	result = (nbr % len) + '0';
+	ft_putchar(result);
+	return (count);
 }
 int	check_format(char indice, va_list ap)
 {
 	int	count;
 
 	count = 0;
-	if (indice == NULL)
-		return (0);
+//	if (indice = NULL)
+//		return (0);
 	else if (indice == 'c')
-		ft_putchar_fd(va_arg(ap, int), 1);
+		ft_putchar(va_arg(ap, int));
 	else if (indice == 'd' || indice == 'i'|| indice == 'u')
 		ft_putnbr_base(va_arg(ap, int), "0123456789");
 	else if (indice == 'x' || indice == 'X')
 		ft_putnbr_base(va_arg(ap, int), "0123456789abcdef");
-	else if (indice == 's')
-		ft_putstr_fd(va_arg(ap, char *), 1);
-//	return (count);
+//	else if (indice == 's')
+//		ft_putstr_fd(va_arg(ap, char *), 1);
+	return (count);
 }
 
 int	ft_printf(const char *format, ...)
@@ -55,5 +84,5 @@ int	ft_printf(const char *format, ...)
 
 int	main()
 {
-	printf("%x\n",ft_printf("%d aywa %d ds\n", 42, 24));
+	ft_printf("%x", 123);
 }
