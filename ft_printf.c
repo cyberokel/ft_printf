db8361a2cd9fd02b2e10d072edb852a2761ebf43
akhalidi <akhalidi@gmail.com>
2026-01-10 05:35:53 +0100

    %x done

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,7 @@
 //#include "libft.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 
 int	ft_strlen(char *s)
 {
@@ -18,25 +18,47 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+int	ft_checkbase(char *base)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (!base || !base[0] || !base[1])
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] <= 32 || base[i] == 127)
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	ft_putnbr_base(int nbr, char *base)
 {
 	int	len;
+	long	n;
 	int	count;
-	char	result;
 
+	n = nbr;
 	len = ft_strlen(base);
 	count = 0;
-	if (nbr < -1)
-		nbr *= -1;
-	if (nbr < len)
+	if (n < -1)
 	{
-		result = nbr % len + '0';
-		ft_putchar(result);
+		n = -n;
+		ft_putchar('-');
 	}
-	if (nbr > len)
-		count += ft_putnbr_base(nbr / len, base);
-	result = (nbr % len) + '0';
-	ft_putchar(result);
+	if (n >= len)
+		count += ft_putnbr_base(n / len, base);
+	ft_putchar(base[n % len]);
 	return (count);
 }
 int	check_format(char indice, va_list ap)
@@ -46,7 +68,7 @@ int	check_format(char indice, va_list ap)
 	count = 0;
 //	if (indice = NULL)
 //		return (0);
-	else if (indice == 'c')
+	if (indice == 'c')
 		ft_putchar(va_arg(ap, int));
 	else if (indice == 'd' || indice == 'i'|| indice == 'u')
 		ft_putnbr_base(va_arg(ap, int), "0123456789");
@@ -73,9 +95,9 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			check_type(format[i], ap);
+			check_format(format[i], ap);
 		}
-		ft_putchar_fd(format[i], 1);
+		ft_putchar(format[i]);
 		count++;
 		i++;
 	}
