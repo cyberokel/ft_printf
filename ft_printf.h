16ca100278464c08fb32893ce9fa5d153bd2b6ae
Abdellatif Khalidi <akhalidi@mi-c1-p1.s19.be>
2026-02-13 18:18:59 +0100

    chouf daba

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: akhalidi <akhalidi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 18:48:30 by akhalidi          #+#    #+#             */
-/*   Updated: 2026/02/01 19:27:01 by akhalidi         ###   ########.fr       */
+/*   Updated: 2026/02/13 18:18:45 by akhalidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 int	ft_putchar(char c);
 int	ft_strlen(char	*s);
 int	ft_printf(const char *format, ...);
-int	ft_putnbr_base(long n, char *base);
+int	ft_putnbr_base(unsigned long n, char *base);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 
